2329d61032b90f7de15798bcb7e8629d94e1b718
Morsy Yaïch dit Lajmi <92680989+Morsy-Yaich@users.noreply.github.com>
2023-08-11 15:47:37 +0100

    Update IRepositoryLivre.cs
    Correct version

--- Repository/IRepositoryLivre.cs
+++ Repository/IRepositoryLivre.cs
@@ -4,20 +4,10 @@ namespace LibraryBook.Repository
 {
     public interface IRepositoryLivre
     {
-<<<<<<< HEAD
         List<Livre> GetAll();
         Livre GetById(int Id);
         void Add(Livre Livre);
         void UpdateById(int Id,Livre Livre);
         bool DeleteById(int Id);
-=======
-        List<Livre> getAll();
-        Livre getById(int Id);
-
-        void Add(Livre Livre);
-        void Update(int Id,Livre Livre);
-        bool Delete(int Id);
->>>>>>> 1b5da80d78c1004023578aaff9e121c4be2e25a2
-
     }
 }
