baf7d678a718cb3ce92c6209583a1bbf06e8c7ea
Morsy Yaïch dit Lajmi <92680989+Morsy-Yaich@users.noreply.github.com>
2023-08-11 15:48:33 +0100

    Update RepositoryLivre.cs
    Correct version.

--- Repository/RepositoryLivre.cs
+++ Repository/RepositoryLivre.cs
@@ -16,38 +16,22 @@ namespace LibraryBook.Repository
             throw new NotImplementedException();
         }
 
-<<<<<<< HEAD
         public bool DeleteById(int Id)
-=======
-        public bool Delete(int Id)
->>>>>>> 1b5da80d78c1004023578aaff9e121c4be2e25a2
         {
             throw new NotImplementedException();
         }
 
-<<<<<<< HEAD
         public List<Livre> GetAll()
-=======
-        public List<Livre> getAll()
->>>>>>> 1b5da80d78c1004023578aaff9e121c4be2e25a2
         {
             throw new NotImplementedException();
         }
 
-<<<<<<< HEAD
         public Livre GetById(int Id)
-=======
-        public Livre getById(int Id)
->>>>>>> 1b5da80d78c1004023578aaff9e121c4be2e25a2
         {
             throw new NotImplementedException();
         }
 
-<<<<<<< HEAD
         public void UpdateById(int Id, Livre Livre)
-=======
-        public void Update(int Id, Livre Livre)
->>>>>>> 1b5da80d78c1004023578aaff9e121c4be2e25a2
         {
             throw new NotImplementedException();
         }
